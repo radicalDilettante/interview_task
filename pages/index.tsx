23225d30c85b915c71fba2cee24c747f2accb5d6
radicalDilettante <wonjun92@gmail.com>
2022-05-05 21:33:31 +1200

    load hooks in index (15min)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,40 @@
 import Head from "next/head";
 import styles from "@/pages/index.module.css";
+import useForm from "hooks/use_form/use_form";
+
+type FinStatus = {
+  hasPartner: boolean;
+  baseSalary: number;
+  secondBaseSalary: number;
+  hasOtherIncome: boolean;
+  otherIncome: number[];
+  hasLoan: boolean;
+  loans: number[];
+  hasCreditCard: boolean;
+  creditCards: number[];
+  deposit: number;
+};
 
 export default function Home() {
+  const initialValues: FinStatus = {
+    hasPartner: false,
+    baseSalary: 0,
+    secondBaseSalary: 0,
+    hasOtherIncome: false,
+    otherIncome: [],
+    hasLoan: false,
+    loans: [],
+    hasCreditCard: false,
+    creditCards: [],
+    deposit: 0,
+  };
+
+  const onSubmit = (values: FinStatus) => {};
+
+  const { values, setValues, handleChange, submitHandle } = useForm<FinStatus>({
+    initialValues,
+    onSubmit,
+  });
+
   return <div className={styles.container}></div>;
 }
